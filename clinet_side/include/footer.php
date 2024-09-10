48ee14e4acdf8fc925ead35a8dfb56be14017f24
medical_store <rajmaheta2008@gmail.com>
2024-09-10 14:28:04 +0530

    update 10/09

--- clinet_side/include/footer.php
+++ clinet_side/include/footer.php
@@ -1,12 +1,3 @@
-
-<div class="footer">
- <p>
-  This website uses cookies to improve your experience. We'll assume you're ok with this, but you can opt-out if you wish.
-  <a href="#">
-   Accept
-  </a>
- </p>
-</div>
 <script href="assets/js/script.js"></script>
-</body>
+</body> 
 </html>
