f5d8ddd776714b978512f15b0025f757863d8744
medical_store <rajmaheta2008@gmail.com>
2024-09-25 21:33:33 +0530

    update 23/09

--- clinet_side/cart.php
+++ clinet_side/cart.php
@@ -1,3 +1,34 @@
 <?php include 'include/header.php' ?>
-
+<div class="p_header text-center" style="justify-content:center !important;">
+    <h1>BASKET</h1>
+</div>
+<div class="container product-page my-5">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">No</th>
+                <th scope="col">Image</th>
+                <th scope="col">product_Name</th>
+                <th scope="col">QTY</th>
+                <th scope="col">Price</th>
+                <th scope="col">Remove</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>1</th>
+                <th><img src="assets/image/p_image.jpg" alt="product name"></th>
+                <th>product_name</th>
+                <th>product_quntity</th>
+                <th>product_price</th>
+                <th><i class="bi bi-x"></i></th>
+            </tr>
+        </tbody>
+    </table>
+    <div class="total_amount">
+        <br><br>
+        <h5 class="text-end">TOTAL AMOUNT --</h5>
+    </div>
+    <div class="update-btn"></div>
+</div>
 <?php include 'include/fotter.php' ?>
